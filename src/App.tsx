75eaf34b76f9e049dd747af2eb9887b1f6065b6d
venzaroblue-maker <venzaroblue@gmail.com>
2026-07-02 23:32:06 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,7 @@ export default function App() {
       <Header />
       <main>
         <Hero />
-        <DenimStory />
+       
         <About />
         <Products />
         <ProductCatalog />
